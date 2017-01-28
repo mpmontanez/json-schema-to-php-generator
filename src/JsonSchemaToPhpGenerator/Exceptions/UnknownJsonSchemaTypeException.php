ef18654b072f747134b4ebcb988a1befab7fe6e5
Matthew Montanez <matthew.montanez@demandmedia.com>
2017-01-28 11:49:06 -0800

    Simple class generation.

---
+++ src/JsonSchemaToPhpGenerator/Exceptions/UnknownJsonSchemaTypeException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace mpmontanez\JsonSchemaToPhpGenerator\Exceptions;
+
+class UnknownJsonSchemaTypeException extends \InvalidArgumentException
+{
+    
+}
--- src/JsonSchemaToPhpGenerator/Generator.php
+++ src/JsonSchemaToPhpGenerator/Generator.php
@@ -2,8 +2,13 @@
 
 namespace mpmontanez\JsonSchemaToPhpGenerator;
 
+use gossi\codegen\generator\CodeGenerator;
+use gossi\codegen\model\PhpClass;
+use gossi\codegen\model\PhpMethod;
+use gossi\codegen\model\PhpParameter;
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
+use mpmontanez\JsonSchemaToPhpGenerator\Exceptions\UnknownJsonSchemaTypeException;
 
 class Generator 
 {
@@ -13,7 +18,13 @@ class Generator
     public function __construct()
     {
         $this->config = [
-            'json-schemas' => ['source-dir' => 'json-schemas'],
+            'json-schemas' => [
+                'source-dir' => 'json-schemas',
+                'dest-dir'   => 'generated-code'
+            ],
+            'generated-code' => [
+                'namespace' => 'Sample\\GeneratedCode\\'
+            ],
             'application' => ['root-dir' => __DIR__ . '/../../']
         ];
 
@@ -23,14 +34,55 @@ class Generator
 
     public function generate()
     {
-        // Read JSON schema file(s).
+        // Empty the destination directory.
+        $this->fileSystem->deleteDir($this->config['json-schemas']['dest-dir']);
+        $this->fileSystem->createDir($this->config['json-schemas']['dest-dir']);
+
+        // Read JSON schema file(s) from a source directory.
         $jsonSchemaFiles = $this->fileSystem->listContents($this->config['json-schemas']['source-dir']);
         foreach ($jsonSchemaFiles as $jsonSchemaFile) {
             $contents = $this->fileSystem->read($jsonSchemaFile['path']);
-            // @todo Parse Json contents and convert into PHP classes.
+            $schema = json_decode($contents, true);
+
+            // Generate code.
+            $code = $this->generatePhpCodeFromSchema($schema);
+
+            // Write .php files to a destination directory.
+            $fileName = $this->determineClassNameFromSchemaTitle($schema['title']) . '.php';
+            $filePath = $this->config['json-schemas']['dest-dir'] . '/' . $fileName;
+            $this->fileSystem->write($filePath, $code);
         }
 
         return true;
     }
 
+    protected function generatePhpCodeFromSchema($schema)
+    {
+        $type = $schema['type'];
+
+        if ($type === 'object') {
+            return $this->generatePhpClassFromSchema($schema);
+        }
+
+        throw new UnknownJsonSchemaTypeException('Unknown schema type: ' . $type);
+    }
+
+    protected function generatePhpClassFromSchema($schema)
+    {
+        $className = $this->determineClassNameFromSchemaTitle($schema['title']);
+
+        $class = new PhpClass();
+        $class
+            ->setQualifiedName($this->config['generated-code']['namespace'] . $className)
+            ->setDescription('Auto-generated class from JSON schema file.')
+        ;
+
+        $generator = new CodeGenerator();
+        return '<?php ' . PHP_EOL . PHP_EOL . $generator->generate($class);
+    }
+
+    protected function determineClassNameFromSchemaTitle($schemaTitle)
+    {
+        return str_replace(' ', '', $schemaTitle);
+    }
 }
